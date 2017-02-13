a8b2e76bbc1f0fb92e798dd5b06d817b8d8684ad
Cygnus Sierra <cygnus-sierra@cygnuss-mac.local>
2017-02-13 16:42:58 +0530

    Initial Commit

---
+++ GoogleMAP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleMAP
+//
+//  Created by Cygnus - Sierra on 2/13/17.
+//  Copyright © 2017 Cygnus - Sierra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
